40256d2fd1b797aa4f213ec4ea54887ec2779134
Nepsoul <nirumagar901@gmail.com>
2025-06-10 22:19:46 +0545

    feat: show all countries list

--- Weather-Or-Not/src/App.jsx
+++ Weather-Or-Not/src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect,useState } from "react";
+import countriesApi from './services/coutries_api'
+
 function App() {
+const [countryData,setCountryData] = useState([])
+
+  useEffect(()=>{
+   countriesApi.getAllCountries().then(
+   data=>{
+   console.log(data,"ctry data")
+   setCountryData(data)
+   })
+   .catch(error=>{
+    console.error('Error fetching countries: ',error)
+   })
+  },[])
   return (
     <>
-      <h1> Weather or Not</h1>
+      <h1>Weather or Not</h1>
+      <p>Your comprehensive weather and air quality companion</p>
+     <h2>Countries List</h2>
+      <ul>
+        {countryData.map(country=>(
+          <li key={country.cca2 ||country.cca3}>{country.name.common}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Weather-Or-Not/src/services/coutries_api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const baseUrl='https://studies.cs.helsinki.fi/restcountries/api/';
+
+const getAllCountries=async()=>{
+    try{
+        const response=await axios.get(`${baseUrl}/all`)
+        console.log(response.data,"response data")
+        return response.data;
+    }catch(error){
+        console.error('Error fetching countries: ',error)
+        throw error; //rethrow the error to be handled by the caller
+    }
+}
+
+const getCountryByName=async(name)=>{
+    try{
+        const response=await axios.get(`${baseUrl}/name/${name}`)
+        return response.data;
+    }catch(error){
+        console.error('Error fetching country by name: ',error)
+        throw error;
+    }
+}
+
+export default {getAllCountries, getCountryByName}
