b3b1a7fe3b743ab11e99dbc0733dc20ad93ba00f
Sanjeev Rai <sunzeevraee@gmail.com>
2025-06-11 17:30:32 +0545

    Some minor changes in fetchWeather.js like remove commented code and console.log()

--- weather-app/src/api/fetchWeather.js
+++ weather-app/src/api/fetchWeather.js
@@ -1,19 +1,9 @@
-// import config from "../config/config";
-// const fetchWeather2 = async (city) => {
-//   const { baseApiUrl, API_KEY } = config;
-//   console.log("hellllllllllsaf", baseApiUrl, API_KEY);
-//   const response = await fetch(
-//     `${baseApiUrl}/weather?q=${city}&appid=${API_KEY}&units=metric`
-//   );
-//   const data = await response.json();
-//   console.log(data);
-//   return data;
-// };
-
+import config from "../config/config";
 const fetchWeather = async (city) => {
+  const { baseApiUrl, API_KEY } = config;
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=26a1c04e3f1b5d57d000395965ceb5f3&units=metric`
+      `${baseApiUrl}/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     const data = await response.json();
     console.log("Fetchweather data", data);
