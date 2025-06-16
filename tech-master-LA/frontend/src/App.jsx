68c48fffae2cf330585e41390c63fd925ea0d47a
ShankarBirTamang <sankar.jt19@gmail.com>
2025-06-16 22:16:08 +0545

    feat: Configured Full Project Structure for Tech Master Learning Assistant
    ## AI prompts used:
    * Help me to design a project structure for fullstack apps that uses mongoDB
    
    ## Manual changes :
    * Installed required packages
    * Configured Tailwind css for frontend
    * Created some folder structure for backend
    
    ## Understanding Check
    * This commit structures full stack apps along with required configuration initially

---
+++ tech-master-LA/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="bg-blue-200 text-center text-red-900 p-4 rounded">
+  Hello Tailwind!
+</div>
+
+    </div>
+  )
+}
+
+export default App
