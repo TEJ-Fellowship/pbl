901c868e3516e33258f63fb83182e19f72c80fa8
Swikar Ramdam <swikarramdam@gmail.com>
2025-07-28 18:44:42 +0545

    just testing 2

--- PBL1/SparkMind/src/App.jsx
+++ PBL1/SparkMind/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Swikar Changed Again</h1>
+      <h1>Just testing 1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
