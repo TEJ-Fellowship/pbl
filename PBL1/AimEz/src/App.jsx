a32143adb670b6b11fb1f4df4ca9ac22aa1e8a5a
projectone <shreeshanjana768@gmail.com>
2025-07-28 20:46:34 +0545

    binita

--- PBL1/AimEz/src/App.jsx
+++ PBL1/AimEz/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>binita hamal</h1>
+      <h1>Anjana Budhathoki</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
