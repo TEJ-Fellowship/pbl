59e3886e7e112ea7eb7dc82179ada3cce4c5990a
ganesh-786 <trump72np@gmail.com>
2025-07-30 10:24:24 +0545

    test: Ganesh is testing

--- PBL1/FocusFlow-Project/frontend/focusflow/src/App.jsx
+++ PBL1/FocusFlow-Project/frontend/focusflow/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Banner from "./components/banner";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>normal website</h1>
+      <Banner />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ PBL1/FocusFlow-Project/frontend/focusflow/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      <h1>Hello, I am ganesh chaudhary!</h1>
+    </div>
+  );
+}
+
+export default Banner;
