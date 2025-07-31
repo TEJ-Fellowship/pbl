e2259df90af7ac8555a0973566e0dd6e285e99da
Lk-Chy <deathreaperzeref@gmail.com>
2025-07-31 13:59:12 +0545

    feat: Navbar
    added navbar feature to homepage

---
+++ PBL1/NourishNow/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <div className='navbar'>
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ PBL1/NourishNow/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="Nav-bar">
+    <span className="Culinary-Campanion"> 
+      Culinary Campanion
+      </span>
+      <div className="part1">
+        <span id="home">Home</span>
+        <span id="Explore">Explore</span>
+        <span id="MyRecipies">MyRecipies</span>
+      </div>
+      <div>
+       <span id="Search">
+        <input placeholder='🔍Search'></input>
+       </span>
+       <span id="bookmark">Favourites</span>
+       <span id="Avatar"></span>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
+
