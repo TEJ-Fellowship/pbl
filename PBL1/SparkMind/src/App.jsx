b4d18c67242b1d2897eecc3297f53c96b80ef5c4
Swikar Ramdam <swikarramdam@gmail.com>
2025-07-31 12:12:04 +0545

    added the router part for quiz

--- PBL1/SparkMind/src/App.jsx
+++ PBL1/SparkMind/src/App.jsx
@@ -1,7 +1,22 @@
-import Home from "./pages/Home";
-function App(){
-  return <Home />
+// import Home from "./pages/homepage/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/homepage/Home";
+import Quiz from "./pages/Quizpage/Quiz";
 
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home/>
+  },
+  {
+    path : "/quiz",
+    element : <Quiz/>
+  },
+])
+
+function App(){
+  return <RouterProvider router = {router}/>
 }
 
 export default App;
--- PBL1/SparkMind/src/pages/homepage/Home.jsx
+++ PBL1/SparkMind/src/pages/homepage/Home.jsx
@@ -1,15 +1,20 @@
 import './Home.css'
-
+import { useNavigate } from 'react-router-dom';
 const Home =()=>{
+    const navigate = useNavigate();
+
+    const handlePlay=()=>{
+        navigate("/quiz")
+    };
+    
     return (
         <div className='homeWrapper'>
         <div className="homeContainer">
             <h1 className='logo'>Spark Mind</h1>
           <p className='tagline'>Play 30s Quiz</p> 
-          <button className='playBtn'>Play</button> 
+          <button className='playBtn' onClick={handlePlay}>Play</button> 
         </div>
         </div>
-
     )
 }
 
