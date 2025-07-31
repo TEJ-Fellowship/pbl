13640b03b17a679e92963010110792abd30ab10d
Swikar Ramdam <swikarramdam@gmail.com>
2025-07-31 10:19:44 +0545

    done with the homepage UI

---
+++ PBL1/SparkMind/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css'
+
+const Home =()=>{
+    return (
+        <div className='homeWrapper'>
+        <div className="homeContainer">
+            <h1 className='logo'>Spark Mind</h1>
+          <p className='tagline'>Play 30s Quiz</p> 
+          <button className='playBtn'>Play</button> 
+        </div>
+        </div>
+
+    )
+}
+
+export default Home;
