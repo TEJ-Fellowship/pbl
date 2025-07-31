7110619642310116338ba1aadd5474fed5cd6d33
projectone <shreeshanjana768@gmail.com>
2025-07-31 11:11:34 +0545

    landing-issue done

---
+++ PBL1/AimEz/src/Navbar.jsx
@@ -0,0 +1,22 @@
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <div>
+            <div className="body">
+            <div className="Nav">
+            <a>Home</a>
+            <a>Categories</a>
+            <a>Contact</a>
+            <a>About Us</a>
+            </div>
+            <h1>AimEZ</h1>
+            <h3>*Your friendly app for goal tracking*</h3>
+            </div>
+         </div>
+        
+        
+    )
+}
+
+export default Navbar
