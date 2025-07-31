b5cf22e4c9f3e493cb2bfdc00503571086a898f8
projectone <shreeshanjana768@gmail.com>
2025-07-31 20:35:08 +0545

    landing-page

---
+++ PBL1/AimEz/src/components/Dashboard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+import { useState } from "react";
+
+function Dashboard() {
+  const [activeSection, setActiveSection] = useState("home");
+ 
+
+  return (
+    <div class="parent" style={{ width: "100vw", height: "100vh", display:"flex" }}>
+      <Sidebar setActiveSection={setActiveSection} />
+
+
+      <div style={{ flexGrow: 1, padding: "20px" }}>
+      {activeSection === 'home' && <h1>🏠 <Home/></h1>}
+        {activeSection === 'goals' && <h1>🎯 Set Your Goals</h1>}
+        {activeSection === 'calendar' && <h1>📅 View Calendar</h1>}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
