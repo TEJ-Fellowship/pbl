26751c1bf8990ca8e87c3ec2c5afca4805afff92
Hari Shankar Limbu <limashok212@gmail.com>
2025-08-01 20:14:29 +0545

    my ashok portion

--- PBL1/SparkMind/src/pages/Quizpage/Quiz.jsx
+++ PBL1/SparkMind/src/pages/Quizpage/Quiz.jsx
@@ -1,25 +1,59 @@
-import './Quiz.css'
-import React from 'react'
-import Quiz_timer from './Quiz_timer'
-function Quiz(){
-return (
-<div className='container'> 
-<div className="question-card">
-     <Quiz_timer/>
-    <h2 className="question-title">What is the capital city of Nepal?</h2>
-    
-    <p className='topic' id='topic'>Social</p>
-    <hr className='underline'></hr>
-    <div className='option-container'>
-    <h3 className='option1'>option1 </h3>
-    <h3 className='option1'>option2 </h3>
-    <h3 className='option1'>option3 </h3>
-    <h3 className='option1'>option4 </h3>
-    <h3 className='next'> Next </h3>
-    </div>
-</div>
-</div>
-)
+import './Quiz.css';
+import React, { useState } from 'react';
+import Quiz_timer from './Quiz_timer';
+import questions from './questions.json';
+
+function Quiz() {
+    const [currentIndex, setCurrentIndex] = useState(3);
+    const [feedback, setFeedback] = useState('');
+    const currentItem = questions[currentIndex];
+
+    // Shuffle logic
+    function shuffleArray(array) {
+        const copied = [...array];
+        for (let i = copied.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [copied[i], copied[j]] = [copied[j], copied[i]];
+        }
+        return copied;
+    }
+
+    // Shuffle only once
+const [ourOptions] = useState(() => shuffleArray(currentItem.options));
+
+    function answerCheck(option) {
+        if (option === currentItem.answer) {
+            setFeedback('Wow! You are correct')
+        }
+        else {
+            setFeedback(`Oops! Correct answer is ${currentItem.answer}`)
+        }
+    }
+
+    return (
+        <div className='container'>
+            <div className="question-card">
+                <Quiz_timer />
+
+                <h2 className="question-title">{currentItem.question}</h2>
+                <p className='topic' id='topic'>{currentItem.topic}</p>
+                <hr className='underline' />
+
+                <div className='option-container'>
+                    <div className='options'>
+                        <h3 className='option1' onClick={() => { answerCheck(ourOptions[0]) }}>{ourOptions[0]}</h3>
+                        <h3 className='option1' onClick={() => { answerCheck(ourOptions[1]) }}>{ourOptions[1]}</h3>
+                        <h3 className='option1' onClick={() => { answerCheck(ourOptions[2]) }}>{ourOptions[2]}</h3>
+                        <h3 className='option1' onClick={() => { answerCheck(ourOptions[3]) }}>{ourOptions[3]}</h3>
+
+                    </div>
+
+                    <h3 className='feedback'>{feedback}</h3>
+                    <h3 className='next'>Next</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Quiz;
