644739046327ff98b2937337d0d2ad69bb752644
Lk-Chy <deathreaperzeref@gmail.com>
2025-08-01 20:12:50 +0545

    feat: add new feathure formpage
    form page was sucessfully  integrated on app.jsx

--- PBL1/NourishNow/src/App.jsx
+++ PBL1/NourishNow/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './Components/Navbar/Navbar'
+import Recipeformpage from './Components/Recipe-form-page/Recipeformpage'
+import "./index.css"
+
+
 
 const App = () => {
   return (
     <>
     <div className='navbar'>
       <Navbar/>
+      <Recipeformpage/>
     </div>
     </>
   )
