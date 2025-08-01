932172c088438c882ed61ffdb48fb83d8769c663
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-01 10:02:51 +0545

    timer feature added for quiz

--- PBL1/SparkMind/src/pages/Quizpage/Quiz.jsx
+++ PBL1/SparkMind/src/pages/Quizpage/Quiz.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Quiz_timer from './Quiz_timer'
 
 const Quiz = () => {
   return (
+    <>
     <div>
       <h1>I am quiz page.</h1>
     </div>
+    <Quiz_timer/>
+    </>
   )
 }
 
