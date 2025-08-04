6cae352f7a026b6c3500ffee904c1de42b871b73
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-08-04 11:47:26 +0545

    chore: route in navbar is added

--- PBL1/NourishNow/src/App.jsx
+++ PBL1/NourishNow/src/App.jsx
@@ -4,16 +4,17 @@ import Navbar from './Components/Navbar/Navbar'
 import Recipeformpage from './Components/Recipe-form-page/Recipeformpage'
 import "./index.css"
 import HomePage from './Components/Home-page/HomePage.jsx'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+    <Router>
     <div className='navbar'>
       <Navbar/>
+      <Recipeformpage/>
     </div>
-        <HomePage />
-    <Recipeformpage/>
+    <HomePage />
     </>
   )
 }
