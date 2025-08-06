72278e3b3069a03128974962553bc5af3aac4d01
mushkan27 <muskanyon@gmail.com>
2025-08-06 11:24:55 +0545

    feat: implement user authentication and income management controllers

---
+++ finance-tracker/server/controllers/authController.js
@@ -0,0 +1,98 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+// register user
+exports.registerUser = async (req, res) => {
+    const { fullName, email, password, profileImageUrl } = req.body;
+
+    if(!fullName || !email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "Please fill all fields"
+        });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "Email already in use"
+            });
+        }
+
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        });
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error registering user"
+        });
+    }
+}
+    
+// login user
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "Please fill all fields"
+        });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if(!user || !(await user.matchPassword(password))){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        });
+    } catch (error) {   
+        res.status(500).json({
+            success: false,
+            message: "Error logging in"
+        });
+    }
+}
+
+// get user info
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json({
+            id: user._id,
+            user
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error getting user info"
+        });
+    }
+}
+
