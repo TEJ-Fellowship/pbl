f61555f7abfad092a42683e2a964cdd759e5756e
mushkan27 <muskanyon@gmail.com>
2025-08-06 11:22:16 +0545

    chore: add server setup with Express, CORS, and initial routes

---
+++ finance-tracker/server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth', authRoutes);
+app.use('/api/income', incomeRoutes);
+
+//Serve uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
