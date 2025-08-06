f107aef98f6f93cb667475fff9a00637607e7378
anish-X <anish.stha527@gmail.com>
2025-08-06 20:00:54 +0545

    fix: routes

--- PBL1/Spendly-Project/frontend/src/App.jsx
+++ PBL1/Spendly-Project/frontend/src/App.jsx
@@ -8,20 +8,23 @@ import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ExpenseForm from "./components/Expense/Expense";
 import { useState } from "react";
+import Dashboard from "./pages/dashboard";
 
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [barActive, setBarActive] = useState(false);
   return (
     <>
       <Router>
         <div className="appContainer">
           <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-          <Sidebar />
+          <Sidebar barActive={barActive} setBarActive={setBarActive}/>
 
           <Routes>
             <Route path="/transaction" element={<TransactionList searchQuery={searchQuery} />}></Route>
             <Route path="/add-expense" element={<ExpenseForm />}></Route>
+            <Route path="/" element={<Dashboard barActive={barActive} />}></Route>
           </Routes>
 
         </div>
