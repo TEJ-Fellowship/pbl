b01a598ab3db2141e38bcda4ff3b51cdbe2762f3
Anu Ale Magar <anualemagar212@gmail.com>
2025-08-07 18:51:50 +0545

    geminicard added

---
+++ PBL1/Eventia-Project/eventia/src/components/GeminiCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/Geminicard.css'
+function GeminiCard() {
+  return (
+    <div className='geminimain'>
+
+        <div className="geminiheader">
+            <h2>Your Summary From Ai</h2>
+        <button  className="crossShoww">
+          X
+        </button>
+      </div>
+      <hr className="tophrrshow" />
+      <div className="content">content from</div>
+    </div>
+  )
+}
+
+export default GeminiCard
--- PBL1/Eventia-Project/eventia/src/components/Sidediv.jsx
+++ PBL1/Eventia-Project/eventia/src/components/Sidediv.jsx
@@ -1,20 +1,29 @@
-import {React, useState} from 'react'
-import CreateButton from './CreateButton'
-import AddEventCard from './AddEventCard'
-import AskGeminiButton from './AskGeminiButton'
-function Sidediv({events,setEvents}) {
-    const [popup,setpopup]=useState(false)
-    function handleClick(){
-
-        setpopup(!popup)
-    }
-    return (
+import { React, useState } from "react";
+import CreateButton from "./CreateButton";
+import AddEventCard from "./AddEventCard";
+import AskGeminiButton from "./AskGeminiButton";
+import GeminiCard from "./GeminiCard";
+function Sidediv({ events, setEvents }) {
+  const [popup, setpopup] = useState(false);
+  function handleClick() {
+    setpopup(!popup);
+  }
+  return (
     <div className="sidediv">
-    <CreateButton handleClick={handleClick} />
-    <AskGeminiButton />
-    {popup && <AddEventCard handleClick={handleClick} events={events} setEvents={setEvents}/>}
-  </div>
-  )
+     
+      <CreateButton handleClick={handleClick} />
+     <GeminiCard />
+      <AskGeminiButton />
+        
+      {popup && (
+        <AddEventCard
+          handleClick={handleClick}
+          events={events}
+          setEvents={setEvents}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Sidediv;
