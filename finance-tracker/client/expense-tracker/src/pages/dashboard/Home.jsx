9a92375675023e3b316ecb75fd358b545ef74590
Libina-Rai <libinarai@gmail.com>
2025-08-11 02:14:43 +0545

    feat: implement dashboard components including income and expense tracking, charts, and modals

--- finance-tracker/client/expense-tracker/src/pages/dashboard/Home.jsx
+++ finance-tracker/client/expense-tracker/src/pages/dashboard/Home.jsx
@@ -1,53 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import DashboardLayout from '../../components/layouts/DashboardLayout'
-import { useUserAuth } from '../../hooks/useUserAuth'
-import { useNavigate } from 'react-router-dom'
-import axiosInstance from '../../utils/axiosInstance'
-import { API_PATHS } from '../../utils/apiPaths'
-import InfoCard from '../../components/Cards/InfoCard'
-import { IoMdCard } from 'react-icons/io'
-import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu'
-import { addThousandsSeparator } from '../../utils/helper'
-import RecentTransactions from '../../components/Dashboard/RecentTransactions'
-
+import React, { useState, useEffect } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import InfoCard from "../../components/Cards/InfoCard";
+import { IoMdCard } from "react-icons/io";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { addThousandsSeparator } from "../../utils/helper";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
+import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
+import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
+import RecentIncome from "../../components/Dashboard/RecentIncome";
 
 const Home = () => {
-  useUserAuth()
+  useUserAuth();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [dashboardData, setDashboardData] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchDashboardData = async () => {
     if (loading) return;
 
-    setLoading(true)
+    setLoading(true);
 
     try {
-
       const response = await axiosInstance.get(
         `${API_PATHS.DASHBOARD.GET_DATA}`
-      )
+      );
       if (response.data) {
-        setDashboardData(response.data)
+        setDashboardData(response.data);
       }
     } catch (error) {
-      console.log('Something went wrong', error)
+      console.log("Something went wrong", error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchDashboardData()
-    return () => { }
-  }, [])
+    fetchDashboardData();
+    return () => {};
+  }, []);
 
   return (
-    <div>
-      <DashboardLayout activeMenu="Dashboard">
-        <div className="my-5 mx-auto">
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto">
         <div
           className='grid grid-cols-1 md:grid-cols-3 gap-6'>
           <InfoCard
@@ -76,16 +78,40 @@ const Home = () => {
 
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 '>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 ">
           <RecentTransactions
             transactions={dashboardData?.RecentTransactions}
             onSeeMore={() => navigate("/expense")}
           />
-          </div>
-        </div>
-      </DashboardLayout>
-    </div>
-  )
-}
 
-export default Home
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+          />
+
+          <ExpenseTransactions
+            transactions={dashboardData?.last30DaysExpenses?.transactions || []}
+            onSeeMore={() => navigate("/expense")}
+          />
+
+          <Last30DaysExpenses
+            data={dashboardData?.last30DaysExpenses?.transactions|| []}
+          />
+
+            <RecentIncomeWithChart
+              data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
+              totalIncome={dashboardData?.totalIncome || 0}
+            />
+
+            <RecentIncome
+              transactions={dashboardData?.last60DaysIncome?.transactions || []}
+              onSeeMore={() => navigate("/income")}
+            />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Home;
