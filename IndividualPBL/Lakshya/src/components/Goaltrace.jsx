ab1ec4de8bdbf0f3531c1a439809ca71b706b62e
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-17 11:19:23 +0545

    feat: files are added

---
+++ IndividualPBL/Lakshya/src/components/Goaltrace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Goaltrace = () => {
+  return (
+    <div>Goaltrace</div>
+  )
+}
+
+export default Goaltrace
---
+++ IndividualPBL/Lakshya/src/components/Themetoggle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Themetoggle = () => {
+  return (
+    <div>Themetoggle</div>
+  )
+}
+
+export default Themetoggle
