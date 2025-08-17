1e56999991ac7fcaad0af46caab214f2801ed7b5
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-17 11:25:09 +0545

    feat: dashboard and stats card features are added

--- IndividualPBL/Lakshya/src/components/Dashboard.jsx
+++ IndividualPBL/Lakshya/src/components/Dashboard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import StatCard from "./StatCard";
+import Career from "../pages/Career.jsx";
+
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+        <div className="p-3 ">
+          <h1 className="font-bold text-3xl text-slate-800/90 dark:text-slate-100/80">Your Career Journey</h1>
+          <p className="text-sm text-slate-700 dark:text-slate-100/80">
+            Track Your Progress, Explore Opportuinities, and achieve your career
+            goals
+          </p>
+        </div>
+        <div className="space-y-6">
+          <StatCard />
+        </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
