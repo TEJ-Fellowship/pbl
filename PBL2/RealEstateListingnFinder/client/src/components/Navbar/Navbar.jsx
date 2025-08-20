5ac3836b0e54ea7553a3499aa3869b7e45d321d7
anish-X <anish.stha527@gmail.com>
2025-08-20 12:44:07 +0545

    feat: Navbar UI

---
+++ PBL2/RealEstateListingnFinder/client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Star, Bell } from "lucide-react";
+const Navbar = () => {
+  return (
+    <>
+      <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-gray-200 px-10 py-3">
+        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-4 text-gray-900">
+            <div className="size-4">
+              <Star className="w-4 h-4 fill-current" />
+            </div>
+            <h2 className="text-gray-900 text-lg font-bold leading-tight tracking-tighter">
+              Mero Ghar
+            </h2>
+          </div>
+          <div className="flex items-center gap-9">
+            <a
+              className="text-gray-900 text-sm font-medium leading-normal hover:text-blue-600 cursor-pointer"
+              href="#"
+            >
+              Explore Property
+            </a>
+            <a
+              className="text-gray-900 text-sm font-medium leading-noraml hover:text-blue-600 cursor-pointer"
+              href="#"
+            >
+              Manage Property
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-1 justify-end gap-8">
+          <button className="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 bg-gray-100 text-gray-900 gap-2 text-sm font-bold  leading-normal  tracking-wide min-w-0 px-2.5 hover:bg-gray-200">
+            <Bell className="w-5 h-5" />
+          </button>
+          <div className="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-10 bg-gradient-to-br from-blue-400 to-purple-500"></div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
