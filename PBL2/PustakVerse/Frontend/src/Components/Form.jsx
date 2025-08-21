65cd842514b8b10b2ea568daf11d0a7d6923eccb
Swikar Ramdam <swikarramdam@gmail.com>
2025-08-21 10:14:55 +0545

    moved back to db.json for backend server

--- PBL2/PustakVerse/Frontend/src/Components/Form.jsx
+++ PBL2/PustakVerse/Frontend/src/Components/Form.jsx
@@ -26,7 +26,7 @@ const Form = ({ onClose, onAddBook }) => {
     };
 
     try {
-      const res = await fetch("http://localhost:3001/api/books", {
+      const res = await fetch("http://localhost:3001/books", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
