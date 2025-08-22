f24e10a4e7d63500161d03c8ff706ee36eb78bba
anish-X <anish.stha527@gmail.com>
2025-08-22 13:09:33 +0545

    feat: property controller and models

---
+++ PBL2/RealEstateListingnFinder/server/models/UserModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type:String,
+        unique: true,
+        required:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+    properties: [{
+        type: Schema.Types.ObjectId, ref: 'Property'
+    }],
+
+    favorites: [{
+        type:Schema.Types.ObjectId, ref: 'Property'
+    }],
+
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+})
+
+export const User = mongoose.model('User', userSchema)
