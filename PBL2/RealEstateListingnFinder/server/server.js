6d9f5e8782fef9f8e95710a93f86daa01a7b10aa
anish-X <anish.stha527@gmail.com>
2025-08-25 21:43:31 +0545

    fix: for uploading images

--- PBL2/RealEstateListingnFinder/server/server.js
+++ PBL2/RealEstateListingnFinder/server/server.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json())
+app.use('/uploads', express.static("uploads"))
 
 
 const PORT = process.env.PORT || 5001;
