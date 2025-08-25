c9f95f57acec5452e0275c4a9910b439a2e4bd91
initxmahesh <maheshchaudhary2057@gmail.com>
2025-08-25 21:59:32 +0545

    feat: home page is added

--- PBL2/RealEstateListingnFinder/client/src/App.jsx
+++ PBL2/RealEstateListingnFinder/client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import SearchPage from "./pages/Search/SearchPage";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/front/landing";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Landing />} />
           <Route path="/manage-property" element={<ManageProperty />} />
           <Route path="/explore" element={<SearchPage />} />
         </Routes>
