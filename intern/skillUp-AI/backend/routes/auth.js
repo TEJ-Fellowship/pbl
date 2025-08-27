624b18b9f6b0858e015eec5da7056a1206426851
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-27 19:47:09 +0545

    Remove: error handler and pass error to middleware

--- intern/skillUp-AI/backend/routes/auth.js
+++ intern/skillUp-AI/backend/routes/auth.js
@@ -1,6 +1,8 @@
+const express = require("express");
+const router = express.Router();
 const User = require("../models/user");
 
-app.post("/api/auth/register", async (request, response, next) => {
+router.post("/register", async (request, response, next) => {
   try {
     const { fullName, email, password } = request.body;
 
@@ -16,21 +18,4 @@ app.post("/api/auth/register", async (request, response, next) => {
   }
 });
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    // Invalid ObjectId format (e.g., malformed MongoDB _id)
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    // Mongoose schema validation failed (invalid or missing data)
-    return response.status(400).json({ error: error.message });
-  } else if (error.name === "MongoServerError" && error.code === 11000) {
-    // Unique constraint violation (duplicate value for a field marked as unique)
-    return response.status(400).json({ error: "Duplicate field value" });
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
+module.exports = router;
