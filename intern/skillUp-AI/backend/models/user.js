2450e1f342fbe827522a1d69243364e23bf6a697
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-27 18:10:11 +0545

    Create: user model for register user and make schema and BD connection

--- intern/skillUp-AI/backend/models/user.js
+++ intern/skillUp-AI/backend/models/user.js
@@ -13,3 +13,33 @@ mongoose
   .catch((error) => {
     console.log(error, "error on mongoDB connection");
   });
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    minlength: 6,
+    maxlength: 20,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // optional but recommended
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, // simple email regex validation
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // optional: set a minimum password length
+  },
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
