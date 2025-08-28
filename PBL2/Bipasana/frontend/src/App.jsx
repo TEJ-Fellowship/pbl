74a11a5b242a1c3998111e19a4d53e6cd53834bf
Anu Ale Magar <anualemagar212@gmail.com>
2025-08-28 13:52:43 +0545

    .env file configured

--- PBL2/Bipasana/frontend/src/App.jsx
+++ PBL2/Bipasana/frontend/src/App.jsx
@@ -11,7 +11,7 @@ import ProtectedRoute from './ProtectedRoute.jsx';
 import Account from './pages/Account.jsx';
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn] = useState(true)
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
   
   return (
     <>
