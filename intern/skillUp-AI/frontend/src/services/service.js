205c5391ac72109c0b90d4eb60798724c0984487
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-30 18:19:38 +0545

    Change :  chaining promice to async await ethod

--- intern/skillUp-AI/frontend/src/services/service.js
+++ intern/skillUp-AI/frontend/src/services/service.js
@@ -1,3 +1,5 @@
+// src/services/service.js
+
 import axios from "axios";
 const url = "http://localhost:3001"; // correct URL
 const validateTokenURL = import.meta.env.VITE_VALIDATE_TOKEN_URL;
@@ -12,21 +14,27 @@ const getAll = () => {
     });
 };
 
-const create = (userDefineUrl, userData) => {
-  return axios
-    .post(userDefineUrl, userData)
-    .then((response) => {
-      return response.data;
-    }) // return response to caller
-    .catch((error) => {
-      return { error: error.response?.data || error.message }; // return error
-    });
+const create = async (userDefineUrl, userData) => {
+  try{
+    const response = await axios.post(userDefineUrl, userData);
+    return response.data;
+  }catch(error){
+    throw new Error (error.response?.data?.message || error?.message)
+  }
+  // return axios
+  //   .post(userDefineUrl, userData)
+  //   .then((response) => {
+  //     return response.data;
+  //   }) // return response to caller
+  //   .catch((error) => {
+  //     return { error: error.response?.data || error.message }; // return error
+  //   });
 };
 
 const validateToken = (token) => {
   return axios
     .get(validateTokenURL, {
-      header: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => {
       return response.data;
