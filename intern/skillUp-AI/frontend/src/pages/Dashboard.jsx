294317b00c1ae33342b6553eebf4c070dce72cdd
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-30 13:19:11 +0545

    Create: landing page only

--- intern/skillUp-AI/frontend/src/pages/Dashboard.jsx
+++ intern/skillUp-AI/frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,20 @@
-const Dashboard=()=>{
-    return(
-        <>
-        <div>
-            <p>Choose a topic to study : </p>
-        </div>
-        
-        </>
-    )
-}
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <p>
+          Choose a topic to study :
+          <select>
+            <option>Select a topic</option>
+            <option>JavaScript</option>
+            <option>React</option>
+            <option>Python</option>
+            <option>HTML/CSS</option>
+          </select>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
