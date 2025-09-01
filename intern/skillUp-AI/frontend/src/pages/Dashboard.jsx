062e2153778818522f9e82a5400eb6792ad14f6b
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-01 22:27:03 +0545

    Delete : topic selecter delete and shift in dashboard page

--- intern/skillUp-AI/frontend/src/pages/Dashboard.jsx
+++ intern/skillUp-AI/frontend/src/pages/Dashboard.jsx
@@ -2,30 +2,42 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext.jsx";
 import Logout from "../components/Auth/Logout.jsx";
-import TopicSelector from "../components/chat/TopicSelector.jsx";
 import ChatInterface from "../components/chat/ChatInterface.jsx";
 import ChatHistory from "../components/chat/ChatHistory.jsx";
+import { useState } from "react";
+
 // import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
+  const [topic, setTopic] = useState("");
 
   // const location = useLocation();
   // const user = location.state?.user;
   const { setIsAuthenticated, user } = useContext(AuthContext);
-  console.log("log name ", user.fullName)
+  console.log("log name ", user.fullName);
   return (
     <>
-      <div className="flex gap-x-7" >
+      <div className="flex gap-x-7">
         <div className="w-1/4">
-          <ChatHistory  user={user}/>
+          <ChatHistory user={user} />
         </div>
-        <div >
+        <div>
           <div className="right-0">
-          <Logout setIsAuthenticated={setIsAuthenticated} />
+            <Logout setIsAuthenticated={setIsAuthenticated} />
           </div>
-          <TopicSelector />
+          <div>
+            <p>
+              Choose a topic to study :
+              <select value={topic} onChange={(e) => setTopic(e.target.value)}>
+                <option value="">Select a topic</option>
+                <option value="JavaScript">JavaScript</option>
+                <option value="React">React</option>
+                <option value="Python">Python</option>
+                <option value="HTML/CSS">HTML/CSS</option>
+              </select>
+            </p>
+          </div>
           <ChatInterface />
-          
         </div>
       </div>
     </>
