0eb2e464b970bc5414bd9c6902e86623de0e013a
winiiee <binita.hamal7@gmail.com>
2025-09-02 13:42:05 +0545

    project setup

---
+++ PBL3/Spindle/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div className='bg-red-500 w-10 h-10'>
+      <h1 className='text-green-500'>tinku</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
