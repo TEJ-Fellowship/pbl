e659e0c9a2c676e15ef7109246f4175109ac215a
anish-X <anish.stha527@gmail.com>
2025-09-02 12:07:02 +0545

    chore: backend initialization

---
+++ PBL3/ripple/backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/", (PORT) => {
+  console.log(`Server Running from ${PORT}`);
+});
