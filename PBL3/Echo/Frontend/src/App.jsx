5d3a2792de9f50d6f6fd117b3efe7a9072edde57
Swikar Ramdam <swikarramdam@gmail.com>
2025-09-03 10:57:37 +0545

    small change

---
+++ PBL3/Echo/Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Recorder from "./Components/Recorder";
+
+function App() {
+  return (
+    <>
+      <Recorder />
+    </>
+  );
+}
+
+export default App;
