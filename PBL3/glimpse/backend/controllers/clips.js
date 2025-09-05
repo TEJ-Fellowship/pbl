2819af3c739d5304a88e45805b91a94494dd7f53
manojrai200 <manojrai9801@gmail.com>
2025-09-05 13:54:09 +0545

    fix

--- PBL3/glimpse/backend/controllers/clips.js
+++ PBL3/glimpse/backend/controllers/clips.js
@@ -1,22 +1,38 @@
+const jwt = require('jsonwebtoken')
 const clipsRouter = require('express').Router()
-const Clip = require('../models/clip')
 const User = require('../models/user')
+const Clip = require("../models/clip")
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 clipsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-  const user = new User({
-    email,
-    passwordHash,
-    name,
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  const clip = new Clip({
+    videoUrl: body.content,
+    important: body.important || false,
+    user: user._id
   })
 
-  const savedUser = await user.save()
+  const savedClip = await clip.save()
+  user.clips = user.notes.concat(savedClip._id)
+  await user.save()
 
-  response.status(201).json(savedUser)
+  response.status(201).json(savedClip)
 })
-
-module.exports = usersRouter
