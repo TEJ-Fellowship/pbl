831e339a9b6f1b470107f86da518f7260f6851c9
anish-X <anish.stha527@gmail.com>
2025-09-05 17:41:03 +0545

    feat: ripple effect on tap

--- PBL3/ripple/frontend/src/App.jsx
+++ PBL3/ripple/frontend/src/App.jsx
@@ -1,23 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Homepage from "./pages/Homepage"
-import LogIn from "./pages/LogIn"
-import SignUp from "./pages/SignUp"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Ripple from "./pages/Ripple";
 
 function App() {
-  
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/LogIn' element={<LogIn />} />
-        <Route path='/SignUp' element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/LogIn" element={<LogIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/ripple" element={<Ripple />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- PBL3/ripple/frontend/tailwind.config.js
+++ PBL3/ripple/frontend/tailwind.config.js
@@ -1,19 +1,25 @@
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class', // important!
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class", // important!
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary-color)',
-        secondary: 'var(--secondary-color)',
-        background: 'var(--background-color)',
-        accent: 'var(--accent-color)',
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        background: "var(--background-color)",
+        accent: "var(--accent-color)",
         text: {
-          primary: 'var(--text-primary)',
-          secondary: 'var(--text-secondary)',
+          primary: "var(--text-primary)",
+          secondary: "var(--text-secondary)",
+        },
+      },
+      keyframes: {
+        rippleEffect: {
+          from: { transform: "scale(0.5,0.5)", opacity: "0.5" },
+          to: { transform: "scale(2.5,2.5)", opacity: "0" },
         },
       },
     },
   },
   plugins: [],
-}
+};
