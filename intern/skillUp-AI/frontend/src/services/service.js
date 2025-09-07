6cdda99ee3d2b6148fafb2cd5f46ea55bf5913b8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-08 01:41:59 +0545

    Create : getAllchats request

--- intern/skillUp-AI/frontend/src/services/service.js
+++ intern/skillUp-AI/frontend/src/services/service.js
@@ -3,10 +3,19 @@
 import axios from "axios";
 const url = "http://localhost:3001"; // correct URL
 const validateTokenURL = import.meta.env.VITE_VALIDATE_TOKEN_URL;
-
+const userChaturl = import.meta.env.VITE_USER_CHATS;
 const getAll = () => {
   return axios
     .get(url)
+    .then((response) => response.data)
+    .catch((error) => {
+      // return error info so component can handle
+      return { error: error.response?.data || error.message };
+    });
+};
+const getAllchats = (userId) => {
+  return axios
+    .get(`${userChaturl}/${userId}`)
     .then((response) => response.data)
     .catch((error) => {
       // return error info so component can handle
@@ -29,11 +38,13 @@ const validateToken = (token) => {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => {
-      console.log(response.data.fullName,"this is fullname from services")
+      console.log(response.data.fullName,"this is fullname from services");
+      console.log(response.data.id,"this is id from services")
+
       return response.data;
     })
     .catch((error) => {
       throw error;
     });
 };
-export default { getAll, create, validateToken };
+export default { getAll, getAllchats, create, validateToken };
