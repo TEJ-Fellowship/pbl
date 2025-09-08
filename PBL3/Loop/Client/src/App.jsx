709ac2d1e08d292a3fb4bb8dbf64aad391b65846
projectone <shreeshanjana768@gmail.com>
2025-09-08 12:36:09 +0545

    landing page

--- PBL3/Loop/Client/src/App.jsx
+++ PBL3/Loop/Client/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import AuthForm from './components/AuthForm';
 import Home from "./components/Home";
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} /> {/* Landing page route */}
       <Route path="/login" element={<AuthForm mode = "login"/>}/>
       <Route path="/signup" element={<AuthForm mode = "signup"/>}/>
       {/* Redirect root → login */}
