fc5deed33a049912a1d0a87b1c29053cd3727750
anish-X <anish.stha527@gmail.com>
2025-09-12 00:30:48 +0545

    feat: ripple services and controllers

---
+++ PBL3/ripple/backend/sockets/rippleSocket.js
@@ -0,0 +1,41 @@
+import rippleService from "../services/rippleService.js";
+import contactService from "../services/contactService.js";
+
+export default function (io) {
+  io.on("connection", (socket) => {
+    console.log("socket id: ", socket.id);
+
+    socket.on("join", (userId) => {
+      socket.join(userId);
+    });
+
+    socket.on("sendRipple", async ({ userId }) => {
+      const ripple = await rippleService.createRipple(userId);
+
+      const globalMessage = `${userId} sends a ripple`;
+
+      if (ripple.visibility.includes("global")) {
+        io.emit("rippleNotification", {
+          fromUser: userId,
+          messsage: globalMessage,
+        });
+      }
+
+      if (ripple.visibility.includes("friends")) {
+        const friends = await contactService.getFriends(userId);
+
+        friends.forEach((friend) => {
+          io.to(friend._id.toString()).emit("sendRipple", {
+            fromUser: userId,
+            message: ripple.message,
+          });
+        });
+        io.emit();
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected", socket.id);
+    });
+  });
+}
