0283c32d470c1d8935dbe56bbb5059db45cd67fc
winiiee <binita.hamal7@gmail.com>
2025-09-19 10:06:17 +0545

    delete feat completed

--- PBL3/Spindle/frontend/src/components/MyPolls.jsx
+++ PBL3/Spindle/frontend/src/components/MyPolls.jsx
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from "react";
+import { TrashIcon } from "@heroicons/react/24/solid";
+
 import { Link } from "react-router-dom";
 import axios from "axios";
 function MyPolls() {
   const [myPolls, setMyPolls] = useState([]);
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"))
+    const storedUser = JSON.parse(localStorage.getItem("user"));
     axios
       .get(`http://localhost:5000/api/createPoll?userId=${storedUser._id}`)
       .then((res) => setMyPolls(res.data))
       .catch((err) => console.log(err));
   }, []);
+
+
+  const handleDelete = async(id)=>{
+    const confirm = window.confirm("Are you sure you want to delete this poll?")
+    if(!confirm) return
+    try {
+      await axios.delete(`http://localhost:5000/api/createPoll/${id}`)
+      const filterPoll = myPolls.filter((p)=>p._id !== id)
+      setMyPolls(filterPoll)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+   
+  }
 
   return (
     <div className="p-6">
@@ -25,24 +44,37 @@ function MyPolls() {
               className="bg-white rounded-2xl shadow-md p-5 hover:shadow-lg transition"
             >
               {/* Poll Title */}
-              <h2 className="text-xl font-semibold text-gray-900 mb-3">
+
+              <div className="flex justify-between items-center mb-3">
+              <h2 className="text-xl font-semibold text-gray-900">
                 {poll.question}
               </h2>
+
+              <button className="text-red-500 hover:text-red-700"
+              onClick={()=> handleDelete(poll._id)}
+              >
+                <TrashIcon className="h-6 w-6" />
+              </button>
+              </div>
 
               {/* View Results Link */}
               <Link
                 to={`../polls/${poll._id || poll.id}/results`}
-                className="text-indigo-600 font-medium hover:underline"
+                className="text-red-600 font-medium hover:underline"
               >
                 View Results →
               </Link>
+
+              
+
+
+
             </div>
           ))}
         </div>
       )}
     </div>
   );
-
 }
 
 export default MyPolls;
