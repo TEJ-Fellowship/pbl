5f7e2aadb7276d33a2af645be9d692cff7760f0e
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-22 12:25:05 +0545

    Get : user from AuthContext

--- intern/skillUp-AI/frontend/src/pages/Quize.jsx
+++ intern/skillUp-AI/frontend/src/pages/Quize.jsx
@@ -1,8 +1,17 @@
-
 import { Link } from "react-router-dom";
 import arrowback from "../../assets/arrow_back.png";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Quize = () => {
+
+    const { user } = useContext(AuthContext);
+
+    useEffect(()=>{
+      console.log(user, "this is user in quize");
+      
+    },[])
+  
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 relative overflow-hidden">
       {/* Background overlay (z-0) */}
@@ -10,11 +19,16 @@ const Quize = () => {
       {/* Main content (z-10) */}
       <div className="relative z-10 flex gap-x-7 p-3 h-screen overflow-hidden">
         <div className="bg-slate-800/80 w-full backdrop-blur-xl rounded-2xl shadow-2xl border border-slate-700/50 relative overflow-hidden">
-        {/* Arrow button (z-20 so it stays clickable) */}
-      <Link to="/dashboard" className="relative z-20">
-        <img alt="arrowback" src={arrowback} className="mt-[2px] ml-[2px]"/>
-      </Link>
+          {/* Arrow button (z-20 so it stays clickable) */}
+          <Link to="/dashboard" className="relative z-20">
+            <img
+              alt="arrowback"
+              src={arrowback}
+              className="mt-[2px] ml-[2px]"
+            />
+          </Link>
           <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-cyan-400 via-violet-400 to-fuchsia-400"></div>
+          <p>Hello</p>
         </div>
       </div>
     </div>
