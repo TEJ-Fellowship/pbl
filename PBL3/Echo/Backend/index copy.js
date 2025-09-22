31e4652b402607d7159a30052394135e65edcb38
swikarramdam <swikarramdam@gmail.com>
2025-09-22 12:25:30 +0545

    deleted unnecessary files and added a gitignore

--- PBL3/Echo/Backend/index copy.js
+++
@@ -1,7 +0,0 @@
-//index.js
-const app = require("./app");
-const config = require("./utils/config");
-
-app.listen(config.port, () =>
-  console.log(`Sever running at http://localhost:${config.port}`)
-);
