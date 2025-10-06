5c3608b8648f6c16c21e100e0841f9b413474de9
Anu Ale Magar <anualemagar212@gmail.com>
2025-10-06 17:09:32 +0500

    main folder setup

---
+++ PBL4/PayPalCustomerSupportAgent/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-start">
+      <div className="w-full max-w-4xl px-6 pt-8">
+        <h1 className="text-red-500 p-1 font-bold text-center">
+          Hello PayPal support system!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
