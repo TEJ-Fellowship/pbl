950e8c91675fd8491fcbbc742d6d860a4781314a
anish-X <anish.stha527@gmail.com>
2025-10-08 09:52:04 +0545

    feat: initial commit

---
+++ PBL4/DiscordCommunitySupportAgent/client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex ml-4 text-3xl font-bold underline"> Hello world! </h1>
+    </>
+  );
+}
+
+export default App;
