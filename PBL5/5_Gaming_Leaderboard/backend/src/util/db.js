cdc658cebbe2634e8eb369fd66bdea3a6311a315
manojrai200 <manojrai9801@gmail.com>
2025-11-13 13:25:44 +0545

    feat: install umzug

--- PBL5/5_Gaming_Leaderboard/backend/src/util/db.js
+++ PBL5/5_Gaming_Leaderboard/backend/src/util/db.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
@@ -8,13 +9,29 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: "postgres",
 });
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const main = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    await runMigrations();
+    console.log("connected to the database");
     sequelize.close();
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error("failed to connect to the database:", error);
   }
 };
 
