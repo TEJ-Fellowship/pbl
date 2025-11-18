ead39dd36f9dfbdb839e1b7a0c4b4d2ebe13e5b4
anish-X <anish.stha527@gmail.com>
2025-11-18 19:13:02 +0545

    feat: socket connection; message saves to db

--- PBL5/2_WhatsAppClone/backend/src/server.js
+++ PBL5/2_WhatsAppClone/backend/src/server.js
@@ -3,12 +3,7 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { handleSocket } from "./infrastructure/websocket/handlers/socketHandler.js";
 import messageRoutes from "../src/interfaces/routes/messageRoutes.js";
-
-// Entry point (HTTP + WS + Kafka consumers)
 import { connectToDatabase } from "./config/postgres.js";
-
-import express from "express";
-import { createServer } from "http";
 
 // Initialize database connection
 const startServer = async () => {
@@ -16,18 +11,16 @@ const startServer = async () => {
     const app = express();
     app.use(express.json());
 
-    const httpServer = createServer(app);
-
     const io = new Server(httpServer, {
       cors: { origin: "*", methods: ["GET", "POST"] },
     });
+
+    const httpServer = createServer(app);
 
     handleSocket(io);
     httpServer.listen(3000, () => console.log("Node server running...."));
 
     app.use("/api/conversation", messageRoutes);
-
-    httpServer.listen(3000, () => console.log("Node server running...."));
 
     // Connect to PostgreSQL database
     await connectToDatabase();
