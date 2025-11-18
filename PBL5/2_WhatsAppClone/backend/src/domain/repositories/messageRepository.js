a406b7582808656df7949edc2064a1aaf3ec4d58
anish-X <anish.stha527@gmail.com>
2025-11-18 12:12:52 +0545

    feat: cassandra saveMessage and getMessage from db

--- PBL5/2_WhatsAppClone/backend/src/domain/repositories/messageRepository.js
+++ PBL5/2_WhatsAppClone/backend/src/domain/repositories/messageRepository.js
@@ -1,2 +1,20 @@
 // Abstract message repository interface
 
+export class IMessageRepository {
+  /*
+    save a message to database
+    (Object) message { conversation_id, message_id, sender_id, content, createdAt}
+  */
+  async saveMessage(message) {
+    throw new Error("Method not implemented.");
+  }
+
+  /*
+    (String) chatId
+    (number) limit
+  */
+
+  async getMessages(chatId, limit) {
+    throw new Error("Method not implemented.");
+  }
+}
--- PBL5/2_WhatsAppClone/backend/src/server.js
+++ PBL5/2_WhatsAppClone/backend/src/server.js
@@ -1,23 +1,39 @@
-// Entry point (HTTP + WS + Kafka consumers)
+// // Entry point (HTTP + WS + Kafka consumers)
 
-import client from "./config/cassandra.js";
+// import client from "./config/cassandra.js";
 
-async function test() {
-  const query = `
-    INSERT INTO messages (conversation_id, message_id, sender_id, content, message_type, status, created_at)
-    VALUES (?, now(), ?, ?, ?, ?, toTimestamp(now()));
-  `;
+// async function test() {
+//   const query = `
+//     INSERT INTO messages (conversation_id, message_id, sender_id, content, message_type, status, created_at)
+//     VALUES (?, now(), ?, ?, ?, ?, toTimestamp(now()));
+//   `;
 
-  await client.execute(query, [
-    "550e8400-e29b-41d4-a716-446655440000",
-    "123e4567-e89b-12d3-a456-426614174000",
-    "Hello from Node.js!",
-    "text",
-    "sent",
-  ]);
+//   await client.execute(query, [
+//     "550e8400-e29b-41d4-a716-446655440000",
+//     "123e4567-e89b-12d3-a456-426614174000",
+//     "Hello from Node.js!",
+//     "text",
+//     "sent",
+//   ]);
 
-  console.log("Inserted!");
-  process.exit();
-}
+//   console.log("Inserted!");
+//   process.exit();
+// }
+// test();
+import { CassandraRepository } from "./infrastructure/db/cassandraRepository.js";
 
-test();
+const message = new CassandraRepository();
+
+await message.saveMessage({
+  conversationId: "550e8400-e29b-41d4-a716-446655440000",
+  messageId: "123e4567-1234-12d3-a456-426614174000",
+  senderId: "123e4567-e89b-12d3-a456-426614174000",
+  content: "Hello from clean architecture!",
+  createdAt: new Date(),
+});
+
+const messages = await message.getMessages(
+  "550e8400-e29b-41d4-a716-446655440000",
+  50
+);
+console.log(messages);
