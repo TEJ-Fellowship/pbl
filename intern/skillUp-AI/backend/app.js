588e8c1c9374fa4d55f9bfd3d1d15210b1970bda
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-19 18:29:58 +0545

    Add : Quizerouter

--- intern/skillUp-AI/backend/app.js
+++ intern/skillUp-AI/backend/app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const authRouter = require("./routes/auth"); // wherever your route file is
 const chatsRouter = require("./routes/chat");
 const allChatsRouter = require("./routes/allchats");
+const quizRouter = require("./routes/quiz");
 const config = require("./utils/config");
 const middleWare = require("./utils/middleware");
 
@@ -30,6 +31,7 @@ app.use(middleWare.requestLogger);
 app.use("/api/auth", authRouter); // prefix all routes in authRouter with /api/auth
 app.use("/api/chat", chatsRouter);
 app.use("/api/user", allChatsRouter);
+app.use("/api/quiz", quizRouter);
 //Global error handler
 app.use(middleWare.errorhandler);
 app.use(middleWare.unknownEndpoint);
