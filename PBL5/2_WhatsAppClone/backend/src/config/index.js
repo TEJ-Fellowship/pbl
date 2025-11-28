28df35d8c008b68b88f962f79d640c192511c48c
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-11-28 14:10:47 +0545

    chors: add related dependencies of redis and initialize it

--- PBL5/2_WhatsAppClone/backend/src/config/index.js
+++ PBL5/2_WhatsAppClone/backend/src/config/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Export all configs
-const DATABASE_URL = process.env.DATABASE_URL;
-console.log;
-export { DATABASE_URL };
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_USERNAME = process.env.REDIS_USERNAME;
+export const REDIS_HOST = process.env.REDIS_HOST;
--- PBL5/2_WhatsAppClone/backend/src/config/redis.js
+++ PBL5/2_WhatsAppClone/backend/src/config/redis.js
@@ -1,2 +1,13 @@
 // Redis configuration
 
+import Redis from 'ioredis';
+import { REDIS_PORT, REDIS_PASSWORD, REDIS_USERNAME, REDIS_HOST } from './index.js';
+
+const redis = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+    username: REDIS_USERNAME,
+})
+
+export default redis;
