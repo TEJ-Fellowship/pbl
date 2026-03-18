cbbb0780f5e6c880035c8b3612302e1e02dd5b3b
mushkan27 <muskanyon@gmail.com>
2026-03-18 16:29:06 +0545

    feat(routing): add React Router with home and auth routes

--- PBL2/Pustakalaya/frontend/src/App.jsx
+++ PBL2/Pustakalaya/frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
 
-function App() {
+const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
     </>
   )
 }
