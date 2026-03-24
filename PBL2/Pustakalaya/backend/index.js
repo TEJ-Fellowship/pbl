2b3a6113205cf615424bcc6044f262d81baf36ee
Nepsoul <nirumagar901@gmail.com>
2026-03-24 13:20:45 +0545

    fix(google-books): set default query parameter for book search API

--- PBL2/Pustakalaya/backend/index.js
+++ PBL2/Pustakalaya/backend/index.js
@@ -25,10 +25,8 @@ app.get("/api/books", async (req, res) => {
      - q (required):string = search term (e.g. "javascript")
      - startIndex (optional):number = pagination offset (default: 0)
     */
-    const { q, startIndex } = req.query;
-    if (!q) {
-      return res.status(400).json({ error: "Query is required" });
-    }
+    const { q = "book", startIndex } = req.query;
+
     const index = startIndex ? startIndex : 0;
     /* - Fetches the books from the Google Books API using Node's global fetch.
     - If Node >= 18, global `fetch` is available.
@@ -36,6 +34,7 @@ app.get("/api/books", async (req, res) => {
     const response = await fetch(
       `https://www.googleapis.com/books/v1/volumes?q=${q}&startIndex=${index}&key=${process.env.GOOGLE_BOOKS_API_KEY}`,
     );
+
     const data = await response.json();
 
     if (!data.items) {
