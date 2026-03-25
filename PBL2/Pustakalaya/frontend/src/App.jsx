b74bc4cd7faece603549526c4e4cc3efc3b7e90b
mushkan27 <muskanyon@gmail.com>
2026-03-25 18:52:52 +0545

    feat(MyShelf): add MyShelf page and integrate into routing

--- PBL2/Pustakalaya/frontend/src/App.jsx
+++ PBL2/Pustakalaya/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Favorites from "./pages/Favorites";
+import MyShelf from "./pages/MyShelf";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/favorites" element={<Favorites />} />
       </Route>
       <Route path="/auth" element={<Auth />} />
+      <Route path="/my-shelf" element={<MyShelf />} />
     </Routes>
   );
 };
