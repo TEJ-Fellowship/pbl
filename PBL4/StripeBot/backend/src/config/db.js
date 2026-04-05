10845afe11bf01dea64726739de6b370c4789dc0
Nepsoul <nirumagar901@gmail.com>
2026-04-05 22:25:40 +0545

    add (docstrings): improve code readability with detailed docstrings

--- PBL4/StripeBot/backend/src/config/db.js
+++ PBL4/StripeBot/backend/src/config/db.js
@@ -1,13 +1,25 @@
+/**
+ * Database configuration
+ * pg: PostgreSQL client library
+ * pgvector: PostgreSQL vector extension library
+ */
 import pg from "pg";
 import pgvector from "pgvector/pg";
 import "dotenv/config";
 
+/** Pool: PostgreSQL connection pool
+ * open connections to the database
+ */
 const { Pool } = pg;
 
+/** ssl: SSL configuration tells the Postgres client to use TLS-encrypted connections to connect to the database
+ * rejectUnauthorized: false to allow self-signed certificates
+ */
 const ssl = { rejectUnauthorized: false };
 
 const pool = process.env.DATABASE_URL
-  ? new Pool({
+  ? /** if DATABASE_URL is set, use it to connect to the database */
+    new Pool({
       connectionString: process.env.DATABASE_URL,
       ssl,
     })
